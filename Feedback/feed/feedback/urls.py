55e9b9248f1e0c37bbbdd4e60513c3ce627c00d1
ZedswevenI <hr@z7i.in>
2025-08-28 21:37:53 +0530

    Update urls.py

--- Feedback/feed/feedback/urls.py
+++ Feedback/feed/feedback/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload, name='upload'),
+    path('', views.upload, name='upload'),
     path('results/<int:batch_id>/', views.results, name='results'),
     path('results/<int:batch_id>/save-remarks/', views.save_remarks, name='save_remarks'),
     path('report/', views.report, name='report'),
