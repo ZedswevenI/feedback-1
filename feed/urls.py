99eb7913ed6b328a2cd341686925c9a9e6fdb4b5
ZedswevenI <hr@z7i.in>
2025-09-01 18:54:13 +0530

    Update urls.py

--- feed/urls.py
+++ feed/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('', include('feedback.urls', namespace='feedback')),  # Include feedback app URLs with namespace
-path('', include('feedback.urls')),
+    path('', include('Feedback.urls')),
 ]
